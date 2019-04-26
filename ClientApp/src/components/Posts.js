ca548b32a93ae14d88a40c0afead93b261c51781
Arnold B. Costamero <a012001164@HO.AD.BDO>
2019-04-26 17:45:01 +0800

    redux integration

--- ClientApp/src/components/Posts.js
+++ ClientApp/src/components/Posts.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react'
 import PostForm from './PostForm'
-import {store} from '../store/'
+import store from '../store/'
 
-class Posts extends Component {
+export class Posts extends Component {
   constructor(props){
     super(props);
 
-    // this.state={
-    //   posts:[]
-    // }
-
+    
   }
 
   componentWillMount(){
-
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res=>res.json())
-    .then(data=>this.setState({posts:data}));
+    console.log(store.getState());
+    // fetch('https://jsonplaceholder.typicode.com/posts')
+    // .then(res=>res.json())
+    // .then(data=>this.setState({posts:data}));
   }
   render() {
-    const postItems=this.props.posts.map(post=>(
+    let items=store.getState();
+  
+    const postItems=items.posts.map(post=>(
       
           <li key={post.id}>
 
--- ClientApp/src/store/index.js
+++ ClientApp/src/store/index.js
@@ -1,22 +1,49 @@
 import{createStore} from 'redux'
-
+import thunkMiddleware from 'redux-thunk';
 const initialState={
-   posts:[]
+   posts:[
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "qui est esse",
+      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    },
+    {
+      "userId": 1,
+      "id": 4,
+      "title": "eum et est occaecati",
+      "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+    }]
 }
 const postReducer=(state=initialState,action)=>{
+   
 
-    console.log('reducer running',action);
-    switch (action.type) {
-        case 'FETCH_POSTS':
-          fetch('https://jsonplaceholder.typicode.com/posts')
-          .then(res=>res.json())
-          .then(data=>{state.posts= data});
-
-        default:
-          return state
-      }
+    console.log('reducer running');
+    //   fetch('https://jsonplaceholder.typicode.com/posts')
+    // .then(res=>res.json())
+    // .then(data=>this.setState({posts:data}));
     return state;
 }
-const store= createStore(postReducer);
+
+export const fetchPosts = () => {
+    return dispatch => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+       .then(response => response.json())
+       .then(json => dispatch({ type: 'FETCH_POSTS', json }))
+    }
+   }
+const store= createStore(postReducer,applyMiddleware(thunkMiddleware));
 
 export default store;
